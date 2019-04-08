0ec897513af981449571ebfddf6a9f343dfa803c
jalil718@gmail.com <jalil718@gmail.com>
2019-04-07 23:55:06 -0400

    Date4/7

---
+++ src/test/facebook/DependsOn.java
@@ -0,0 +1,63 @@
+package test.facebook;
+
+import org.testng.annotations.Test;
+
+public class DependsOn {
+
+	@Test
+	public void openBrowser(){
+		System.out.println("This will excute first");
+	}
+
+	@Test (dependsOnMethods = { "openBrowser" })
+	public void SignIn() {
+		// driver.finElement ()
+		System.out.println("This will execute second");
+	}
+
+	@Test (dependsOnMethods = { "SignIn" })
+	public void LogOut() {
+		System.out.println("This will execute third (Log Out)");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
